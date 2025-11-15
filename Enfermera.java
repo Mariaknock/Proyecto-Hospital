9d1d0a43ed361f4055651c1a437b4b5ca4be3ac2
gabrielss-web <gsalaiss14@gmail.com>
2025-11-15 17:40:23 -0600

    "Clase enfermera base"

--- Enfermera.java
+++ Enfermera.java
@@ -1,6 +1,18 @@
 public class Enfermera extends Empleado {
 
-    public Enfermera
+    public Enfermera(int idEmpleado,String nombre,String apellido,String area){
+        super(idEmpleado,nombre,apellido,area);
+    }
+
+    public void asistirPaciente(Paciente pacientePrueba){
+        System.out.println("El enfermera " + getNombre() + "esta asistiendo al paciente " + pacientePrueba.getNombre());
+
+    }
+
+    @Override
+    public void mostrarInformacion(){
+        super.mostrarInformacion();
+    }
 
 
     
