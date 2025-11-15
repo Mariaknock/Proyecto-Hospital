b4f664b4db0f27fda31fe28bd0d09997925667f6
gabrielss-web <gsalaiss14@gmail.com>
2025-11-15 17:50:18 -0600

    "cambios clase recepcionista"

--- Recepcionista.java
+++ Recepcionista.java
@@ -1,5 +1,19 @@
-public class Recepcionista {
+public class Recepcionista extends Empleado {
 
+    public Recepcionista (int idEmpleado,String nombre,String apellido,String area,String turno){
+        super(idEmpleado,nombre,apellido,area,turno);
+    }
+
+    public void agendarCita(){
+        System.out.println(getNombre() + "del turno " + getTurno() + "esta agendando una cita");
+
+
+    }
     
+    @Override
+    public void mostrarInformacion(){
+        System.err.println("Cargo -> Recepcionista");
+        super.mostrarInformacion();
+    }
 
 }
