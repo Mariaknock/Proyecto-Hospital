fe62dfa12d35963f6eb0832f34882aa846779a94
gabrielss-web <gsalaiss14@gmail.com>
2025-11-15 17:21:23 -0600

    "cambios en empleado"

--- Empleado.java
+++ Empleado.java
@@ -1,7 +1,42 @@
 public class Empleado {
     private String nombre;
     private String apellido;
-    private int idPaciente;
+    private int idEmpleado;
     private String area;
     
+    public Empleado(int idEmpleado, String nombre, String apellido, String area){
+        this.idEmpleado=idEmpleado;
+        this.nombre= nombre;
+        this.apellido= apellido;
+        this.area= area;
+
+    }
+
+
+    public void mostrarInformacion(){
+        System.out.println("ID: " + idEmpleado);
+        System.out.println("Nombre " + nombre + " " + apellido);
+        System.out.println("Area: "+ area);
+
+    }
+
+    public String getNombre(){
+        return nombre;
+    }
+
+    public int getidEmpleado(){
+        return idEmpleado;
+    }
+
+    public String getApellido(){
+        return apellido;
+    }
+
+    public String getArea(){
+        return area;
+    }
+
+
+
+
 }
