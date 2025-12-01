a18cefde050e3ecbec1e5d484c2d33a5fa0254a2
Mariaknock <marcmillalun@gmail.com>
2025-11-30 23:48:55 -0600

    scn

--- paciente/GestorPaciente.java
+++ paciente/GestorPaciente.java
@@ -147,7 +147,6 @@ public class GestorPaciente {
 
         System.out.println("Ingresa el nombre del paciente: ");
         String nombre=scanner.nextLine();
-        scanner.nextLine();
         System.out.println("Ingresa la edad: ");
         int edad=scanner.nextInt();
         scanner.nextLine();
